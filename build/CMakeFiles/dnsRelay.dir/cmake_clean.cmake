1a94eb677e7b99814d041859011b43cf15a0eb26
Wayne <arthassum@gmail.com>
2021-05-14 23:18:01 +0800

    first commit

---
+++ build/CMakeFiles/dnsRelay.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dnsRelay.dir/dns0.c.obj"
+  "CMakeFiles/dnsRelay.dir/dns0.c.obj.d"
+  "bin/dnsRelay.exe"
+  "bin/dnsRelay.exe.manifest"
+  "bin/dnsRelay.pdb"
+  "lib/libdnsRelay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dnsRelay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
